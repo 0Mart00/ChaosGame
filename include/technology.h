f574a51d524dcfa122f45bf1ebe5a4b8f23752ff
0Mart00 <nyiri.martin.101@gmail.com>
2026-05-09 15:55:37 +0200

    Add: technology

---
+++ include/technology.h
@@ -0,0 +1,53 @@
+Technology List
+
+Nanobot Repair
+
+Fusion Reactor
+
+Neural Link
+
+Plasma Coating
+
+Antimatter Engine
+
+Quantum Computing
+
+Cyborg Augmentation
+
+Orbital Strike
+
+Shield Overload
+
+#ifndef TECHNOLOGY_H
+#define TECHNOLOGY_H
+
+#include "raylib.h"
+#include <stdbool.h>
+
+typedef enum {
+    TECH_NANOBOT_REPAIR, TECH_FUSION_REACTOR, TECH_NEURAL_LINK, 
+    TECH_PLASMA_COATING, TECH_ANTIMATTER_ENGINE, TECH_QUANTUM_COMPUTING,
+    TECH_CYBORG_AUGMENTATION, TECH_ORBITAL_STRIKE, TECH_SHIELD_OVERLOAD,
+    TECH_BIO_LUMINESCENCE, TECH_LASER_GUIDANCE, TECH_CRYO_STORAGE,
+    TECH_JETPACK_PROPULSION, TECH_SMART_AMMO, TECH_HOLOGRAPHIC_DECOY,
+    TECH_GENETIC_ENGINEERING, TECH_SUBTERRANEAN_DRILL, TECH_GAMMA_RAY_BURST,
+    TECH_EXOSKELETON_SUIT, TECH_ATMOSPHERE_PROCESSOR, TECH_WARP_DRIVE,
+    TECH_SONIC_DAMPENER, TECH_MAGNETIC_RAILGUN, TECH_AI_AUTOPILOT,
+    TECH_THERMAL_VISION, TECH_ENERGY_HARVESTING, TECH_STEALTH_PLATING,
+    TECH_TELEPATHIC_UPLINK, TECH_MATTER_SYNTHESIS, TECH_FORCE_FIELD_DOME,
+    TECH_COUNT
+} TechType;
+
+typedef struct {
+    TechType type;
+    const char* name;
+    int researchCost;    // Kutatási pont vagy nyersanyag igény
+    bool isResearched;   // Ki van-e fejlesztve
+    int requiredLevel;   // Szükséges főhadiszállás szint
+} Technology;
+
+void InitTechSystem(void);
+void ResearchTech(TechType type);
+bool IsTechActive(TechType type);
+
+#endif
---
+++ src/modules/m_technology.c
@@ -0,0 +1,91 @@
+#include "technology.h"
+
+// TECH_NANOBOT_REPAIR: Az egységek és épületek passzív, lassú öngyógyítása harcon kívül.
+void Tech_NanobotRepair() {}
+
+// TECH_FUSION_REACTOR: Drasztikusan megnöveli az alapvető energia-termelést.
+void Tech_FusionReactor() {}
+
+// TECH_NEURAL_LINK: Gyorsabb egység-reakcióidő és megnövelt látótávolság.
+void Tech_NeuralLink() {}
+
+// TECH_PLASMA_COATING: Plusz páncélzat a lövedékek ellen, elnyeli a sebzés egy részét.
+void Tech_PlasmaCoating() {}
+
+// TECH_ANTIMATTER_ENGINE: Minden jármű és űrhajó végsebességének növelése.
+void Tech_AntimatterEngine() {}
+
+// TECH_QUANTUM_COMPUTING: Felgyorsítja a további technológiák kutatási idejét.
+void Tech_QuantumComputing() {}
+
+// TECH_CYBORG_AUGMENTATION: Gyalogsági egységek HP-jának és erejének növelése.
+void Tech_CyborgAugmentation() {}
+
+// TECH_ORBITAL_STRIKE: Új aktív képesség: pusztító csapás az égből a térkép bármely pontjára.
+void Tech_OrbitalStrike() {}
+
+// TECH_SHIELD_OVERLOAD: A pajzsok felrobbanhatnak megsemmisüléskor, sebezve a támadót.
+void Tech_ShieldOverload() {}
+
+// TECH_BIO_LUMINESCENCE: Az egységek fényt bocsátanak ki, eloszlatva a "háború ködét".
+void Tech_BioLuminescence() {}
+
+// TECH_LASER_GUIDANCE: Megnövelt pontosság és hatótáv távolsági fegyvereknél.
+void Tech_LaserGuidance() {}
+
+// TECH_CRYO_STORAGE: Nyersanyag-tárolási limit növelése és veszteségek csökkentése.
+void Tech_CryoStorage() {}
+
+// TECH_JETPACK_PROPULSION: Gyalogság számára lehetővé teszi rövid távolságok átrepülését.
+void Tech_JetpackPropulsion() {}
+
+// TECH_SMART_AMMO: A lövedékek kis mértékben követik a mozgó célpontokat.
+void Tech_SmartAmmo() {}
+
+// TECH_HOLOGRAPHIC_DECOY: Épületek képesek hamis képmást vetíteni magukról.
+void Tech_HolographicDecoy() {}
+
+// TECH_GENETIC_ENGINEERING: Biológiai egységek gyorsabb kiképzése.
+void Tech_GeneticEngineering() {}
+
+// TECH_SUBTERRANEAN_DRILL: Föld alatti nyersanyagforrások hatékonyabb kinyerése.
+void Tech_SubterraneanDrill() {}
+
+// TECH_GAMMA_RAY_BURST: Sugárfegyverek átütő erejének és területsebzésének növelése.
+void Tech_GammaRayBurst() {}
+
+// TECH_EXOSKELETON_SUIT: Gyalogsági egységek teherbírásának és sebességének növelése.
+void Tech_ExoskeletonSuit() {}
+
+// TECH_ATMOSPHERE_PROCESSOR: Lassítja a környezeti ártalmakat és javítja a látási viszonyokat.
+void Tech_AtmosphereProcessor() {}
+
+// TECH_WARP_DRIVE: Lehetővé teszi az azonnali ugrást távoli szektorok között.
+void Tech_WarpDrive() {}
+
+// TECH_SONIC_DAMPENER: Az egységek mozgása nem jelenik meg az ellenséges radaron.
+void Tech_SonicDampener() {}
+
+// TECH_MAGNETIC_RAILGUN: Fizikai alapú fegyverek lövedéksebességének drasztikus növelése.
+void Tech_MagneticRailgun() {}
+
+// TECH_AI_AUTOPILOT: Az egységek okosabban vonulnak fedezékbe, ha nem kapnak parancsot.
+void Tech_AIAutopilot() {}
+
+// TECH_THERMAL_VISION: Láthatóvá teszi a Stealth (lopakodó) egységeket.
+void Tech_ThermalVision() {}
+
+// TECH_ENERGY_HARVESTING: A kilőtt lövedékek energiájának egy részét visszanyeri a hálózat.
+void Tech_EnergyHarvesting() {}
+
+// TECH_STEALTH_PLATING: Passzív álcázás járművek számára álló helyzetben.
+void Tech_StealthPlating() {}
+
+// TECH_TELEPATHIC_UPLINK: Globális egységirányítás: nincs szükség parancsnoki hatótávra.
+void Tech_TelepathicUplink() {}
+
+// TECH_MATTER_SYNTHESIS: Energiából képes alapvető építőanyagokat (fém, kristály) gyártani.
+void Tech_MatterSynthesis() {}
+
+// TECH_FORCE_FIELD_DOME: Fix védelmi épület, amely ernyőt von a bázis fölé.
+void Tech_ForceFieldDome() {}
